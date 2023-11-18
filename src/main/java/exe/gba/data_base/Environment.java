af693ef2a9db645dbb0191cb72735827e08e1708
gba-exe <gabriel.baraujo@sptech.school>
2023-11-18 20:28:52 -0300

    feat(): added tasks to save data in the background

--- src/main/java/exe/gba/data_base/Environment.java
+++ src/main/java/exe/gba/data_base/Environment.java
@@ -11,6 +11,7 @@ import exe.gba.data_managing.data_access.RegisterDao;
 import exe.gba.menu.Menu;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Environment {
@@ -29,48 +30,19 @@ public class Environment {
         JdbcTemplate con = database.getConnection();
         CategoryDao.setCon(con);
         RegisterDao.setCon(con);
-        createH2Database(con);
+        H2.createDatabase(con);
     }
 
     public static Database getDatabase() {
-        configureEnvironment();
+        if (Objects.isNull(database)){
+
+            configureEnvironment();
+        }
 
         return database;
     }
 
     public static void setDatabase(Database database) {
         Environment.database = database;
-    }
-
-    public static void createH2Database(JdbcTemplate con){
-        if (database instanceof H2){
-
-            con.execute("drop table if exists tb_register");
-            con.execute("drop table if exists tb_category");
-
-
-            con.execute("""
-                    CREATE TABLE tb_category (
-                    category_id int primary key not null auto_increment,
-                    name varchar(45) not null,
-                    description varchar(100) not null,
-                    data_unit varchar(20) not null 
-                    );
-                    """);
-
-            con.execute("""
-                    CREATE TABLE tb_register (
-                   register_id int primary key not null auto_increment,
-                   register_value double precision not null,
-                   fk_category int not null,
-                   foreign key (fk_category) references tb_category(category_id)
-                   );
-                    """);
-
-            CategoryDao.insertCategory(new AvailableStorage(null, "Available Storage","Available Storage of all volumes", "GB"));
-            CategoryDao.insertCategory(new TotalStorage(null, "Total Storage","Sumo of Storage of all volumes","GB"));
-            CategoryDao.insertCategory(new CpuUsage(null, "CPU Usage","Usage of processor resources","%"));
-            CategoryDao.insertCategory(new RamUsage(null, "RAM Usage","Usage of RAM resources","%"));
-        }
     }
 }
--- src/main/java/exe/gba/data_base/connection/H2.java
+++ src/main/java/exe/gba/data_base/connection/H2.java
@@ -1,5 +1,11 @@
 package exe.gba.data_base.connection;
 
+import exe.gba.data_base.Environment;
+import exe.gba.data_managing.data_access.CategoryDao;
+import exe.gba.monitorable.categories.AvailableStorage;
+import exe.gba.monitorable.categories.CpuUsage;
+import exe.gba.monitorable.categories.RamUsage;
+import exe.gba.monitorable.categories.TotalStorage;
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.jdbc.core.JdbcTemplate;
 
@@ -13,5 +19,37 @@ public class H2 extends Database {
             dataSource.setPassword("");
 
             this.setConnection(new JdbcTemplate(dataSource));
+    }
+
+    public static void createDatabase(JdbcTemplate con) {
+        if (Environment.getDatabase() instanceof H2) {
+
+            con.execute("drop table if exists tb_register");
+            con.execute("drop table if exists tb_category");
+
+
+            con.execute("""
+                    CREATE TABLE tb_category (
+                    category_id int primary key not null auto_increment,
+                    name varchar(45) not null,
+                    description varchar(100) not null,
+                    data_unit varchar(20) not null 
+                    );
+                    """);
+
+            con.execute("""
+                    CREATE TABLE tb_register (
+                    register_id int primary key not null auto_increment,
+                    register_value double precision,
+                    fk_category int not null,
+                    foreign key (fk_category) references tb_category(category_id)
+                    );
+                      """);
+
+            CategoryDao.insertCategory(new AvailableStorage(null, "Available Storage","Available Storage of all  volumes", "GB"));
+            CategoryDao.insertCategory(new TotalStorage(null, "Total Storage","Sumo of Storag e of all volumes","GB"));
+            CategoryDao.insertCategory(new CpuUsage(null, "CPU Usage","Usage of pr ocessor resources","%"));
+            CategoryDao.insertCategory(new RamUsage(null, "RAM Usage","Usage of RAM resources","%"));
+        }
     }
 }
